8ec29f1a2da01b0a3bdf35c47605e9281623c004
EmanAlOmar <eaa6167@rit.edu>
2019-01-10 07:16:18 -0800

    Add two methods and attribute in a created class

--- src/Test10/Class10.java
+++ src/Test10/Class10.java
@@ -1,5 +1,17 @@
 package Test10;
 
 public class Class10 {
+	
+	public int count = 0; 
+	
+	public void printName()
+	{
+		System.out.println("Print Name");
+	}
+	
+	public void printNumber()
+	{
+		System.out.println("Print Number");
+	}
 
 }
